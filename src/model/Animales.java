b8029be3da9283fa78f03fe68989e28c93a129a5
JuanPabloLopezParedes <juanpablo.lopezp.2006@gmail.com>
2025-04-28 09:28:48 -0500

    feat: Metodo Guardar ANIMALES en un archivo Json

--- src/model/Animales.java
+++ src/model/Animales.java
@@ -1,27 +1,102 @@
 
 package model;
 
-import java.util.Date;
-
-
 public class Animales {
     private String nombre;
     private int idAnimal;
-    private String especia;
-    private int edad;
+    private String peso;
+    private String especie;
     private String sexo;
-    private boolean estadoSalud;
+    private String estadoSalud;
     private String ubicacion;
+    private String habitat;
+    private String cuidadorAsignado;
 
-    public Animales(String nombre, int idAnimal, String especia, int edad, String sexo, boolean estadoSalud, String ubicacion) {
+    public Animales(String nombre, int idAnimal, String peso, String especie, String sexo, String estadoSalud, String ubicacion, String habitat, String habitat1) {
         this.nombre = nombre;
         this.idAnimal = idAnimal;
-        this.especia = especia;
-        this.edad = edad;
+        this.especie = especie;
         this.sexo = sexo;
         this.estadoSalud = estadoSalud;
         this.ubicacion = ubicacion;
+        this.peso = peso;
+        this.habitat = habitat;
+        this.cuidadorAsignado = cuidadorAsignado;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(int idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getEstadoSalud() {
+        return estadoSalud;
+    }
+
+    public void setEstadoSalud(String estadoSalud) {
+        this.estadoSalud = estadoSalud;
+    }
+
+    public String getCuidadorAsignado() {
+        return cuidadorAsignado;
+    }
+
+    public void setCuidadorAsignado(String cuidadorAsignado) {
+        this.cuidadorAsignado = cuidadorAsignado;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+    
+    
     
     public void mostrarInformacion(){
         
@@ -38,6 +113,18 @@ public class Animales {
     public void buscarAnimal(){
         
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 }
